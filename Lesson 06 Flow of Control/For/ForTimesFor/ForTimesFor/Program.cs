0af4bd890fc192c68f67c9d5b7dbbfdd275431a0
ayotechnology31 <ayotechnology31@gmail.com>
2020-01-29 18:10:04 -0600

    forpractices

---
+++ Lesson 06 Flow of Control/For/ForTimesFor/ForTimesFor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ForTimesFor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input;
+			int guess;
+			int total = 0;
+
+			Console.WriteLine("Which times table shall I recite? 5");
+			for (int x = 1; x < 16; x++)
+			{
+				Console.WriteLine("What is " + x + " * 5 is: ");
+				input = Console.ReadLine();
+				if (int.TryParse(input, out guess))
+				{
+					if (guess == x * 5)
+					{
+						Console.WriteLine("Correct!");
+						total++;
+					}
+					else
+					{
+						Console.WriteLine("Sorry no, the answer is: {0}", x * 5);
+					}
+				}
+				//Console.WriteLine(x + "  * 5 is: {0}", x * 5);
+			}
+			Console.WriteLine("You got " + total + " correct.");
+		}
+	}
+}
--- Lesson 06 Flow of Control/Whiles and Dos/LazyTeenager/LazyTeenager/Program.cs
+++ Lesson 06 Flow of Control/Whiles and Dos/LazyTeenager/LazyTeenager/Program.cs
@@ -7,18 +7,36 @@ namespace LazyTeenager
 		static void Main(string[] args)
 		{
 			
-			int clean = 0;
 			Random chance = new Random();
-			clean = chance.Next(15);
+			int clean = chance.Next(15);
 
+			Console.Write("Did you clean your room?");
+			bool cleaned;
+			
+			if(Console.ReadLine().Equals("y"))
+			{
+				cleaned = true;
+			}
+			else
+			{
+				cleaned = false;
+			}
+
+			bool x = false;
 			do
 			{
-				clean = 1;
+				x = true;
 				break;
-			} while (clean);
+			} while (cleaned);
 
-
-			Console.WriteLine("Clean your room!! That's IT, I'm doing it!!! YOU'RE GROUNDED AND I'M TAKING YOUR XBOX!");
+			if(clean*5 != cleaned)
+			{
+				Console.WriteLine("Clean your room!!");
+			}
+			else
+			{
+				Console.WriteLine("Clean your room!! That's IT, I'm doing it!!! YOU'RE GROUNDED AND I'M TAKING YOUR XBOX!");
+			}
 		}
 	}
 }
